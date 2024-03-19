7340fdd15befefcc2ebc384566406ff369a84db9
harish <harishkumarreddy321@gmail.com>
2024-03-19 19:11:50 +0530

    added validation And created a secrity config file

---
+++ src/main/java/com/finalproject/hotelbookingsystem/config/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.finalproject.hotelbookingsystem.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(e->e.disable());
+        httpSecurity.cors(e->e.disable());
+        httpSecurity.authorizeHttpRequests(authRequest ->{
+                    authRequest
+                            //.requestMatchers("/","").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/hotel-api/v1/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers(HttpMethod.DELETE,"/hotel-api/v1/**").hasAnyAuthority("ADMIN")
+                        //    .requestMatchers("").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .anyRequest().authenticated();
+                }).sessionManagement(Customizer.withDefaults())
+                .authenticationProvider(daoAuthenticationProvider())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return  daoAuthenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(List<AuthenticationProvider> authenticationProviders) {
+        return new ProviderManager(authenticationProviders);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/finalproject/hotelbookingsystem/dto/RoomDto.java
+++ src/main/java/com/finalproject/hotelbookingsystem/dto/RoomDto.java
@@ -1,11 +1,13 @@
 package com.finalproject.hotelbookingsystem.dto;
 
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 public class RoomDto {
+    @NotEmpty(message = "Room Type can not be empty. Enter room Type")
     private String roomType;
     @ManyToOne
     private HotelDto hotelDto;
