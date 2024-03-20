5a3ffbe770e383527a86c0c6ebbfcfab6d310516
harish <harishkumarreddy321@gmail.com>
2024-03-21 01:02:12 +0530

    added security

---
+++ src/main/java/com/finalproject/hotelbookingsystem/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.finalproject.hotelbookingsystem.repository;
+
+import com.finalproject.hotelbookingsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    public User getUserByUsername(String username);
+}
---
+++ src/main/java/com/finalproject/hotelbookingsystem/service/UserService.java
@@ -0,0 +1,7 @@
+package com.finalproject.hotelbookingsystem.service;
+
+import com.finalproject.hotelbookingsystem.dto.UserDto;
+
+public interface UserService {
+    public UserDto createUser(UserDto userDto);
+}
