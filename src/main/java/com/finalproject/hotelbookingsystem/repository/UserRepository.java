75deed42b528c69125a82f3c6d1b018867f84cbf
harishkumarreddy3 <145240971+harishkumarreddy3@users.noreply.github.com>
2024-03-21 19:12:26 +0530

    Update UserRepository.java

--- src/main/java/com/finalproject/hotelbookingsystem/repository/UserRepository.java
+++ src/main/java/com/finalproject/hotelbookingsystem/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.finalproject.hotelbookingsystem.repository;
 
 import com.finalproject.hotelbookingsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     public User getUserByUsername(String username);
 }
