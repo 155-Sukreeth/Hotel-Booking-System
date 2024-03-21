009b40b8b441654ee508cbe9ebd2a297ef4e6040
harishkumarreddy3 <145240971+harishkumarreddy3@users.noreply.github.com>
2024-03-21 19:08:58 +0530

    Update and rename SecurityConfig.java to AppSecurityConfig.java

--- src/main/java/com/finalproject/hotelbookingsystem/config/security/AppSecurityConfig.java
+++ src/main/java/com/finalproject/hotelbookingsystem/config/security/AppSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.finalproject.hotelbookingsystem.config.security;
 
+import com.finalproject.hotelbookingsystem.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,20 +22,31 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-
+public class AppSecurityConfig {
+    @Autowired
+    private JwtFilter jwtFilter;
+    @Autowired
+    private CustomUserDetailsService customeUserDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(e->e.disable());
         httpSecurity.cors(e->e.disable());
         httpSecurity.authorizeHttpRequests(authRequest ->{
                     authRequest
-                            //.requestMatchers("/","").permitAll()
-                            .requestMatchers(HttpMethod.POST,"/hotel-api/v1/**").hasAnyAuthority("ADMIN")
-                            .requestMatchers(HttpMethod.DELETE,"/hotel-api/v1/**").hasAnyAuthority("ADMIN")
-                        //    .requestMatchers("").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers("/user-api/v1/users","/user-api/v1/users/login").permitAll()
+                            .requestMatchers("/hotel-api/v1/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers("/room-api/v1/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers(HttpMethod.GET,"/bookings/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers(HttpMethod.PUT,"/bookings/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.POST,"/bookings/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.DELETE,"/bookings/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.DELETE,"/customer/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.GET,"/customer/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers(HttpMethod.PUT,"/customer/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.POST,"/customer/**").hasAnyAuthority("ADMIN","CUSTOMER")
                             .anyRequest().authenticated();
                 }).sessionManagement(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(daoAuthenticationProvider())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
@@ -43,6 +55,8 @@ public class SecurityConfig {
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+        System.out.println("custom service object "+customeUserDetailsService);
+        daoAuthenticationProvider.setUserDetailsService(customeUserDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return  daoAuthenticationProvider;
     }
